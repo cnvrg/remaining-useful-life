500195ff5f03a1f0f2bfd56a131646462e874bab
Harshul-cnvrg <108051386+Harshul-cnvrg@users.noreply.github.com>
2022-11-03 12:29:57 +0530

    Update predict.py

--- inference/predict.py
+++ inference/predict.py
@@ -1,31 +1,38 @@
 '''Endpoint'''
-# pylint: disable=E0611
-# pylint: disable=E0401
-# pylint: disable=C0103
-# pylint: disable=C0411
-# pylint: disable=C0415
-# pylint: disable=C0301
-
-#import base64
-import requests
-import pathlib
-from sklearn.preprocessing import StandardScaler
-from scipy.spatial.distance import pdist, squareform
+import argparse
+import os
+import random
+import itertools
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import warnings
-import os
 warnings.filterwarnings(action='ignore')
+import pickle
 
+from scipy.spatial.distance import pdist, squareform
+from sklearn.metrics import confusion_matrix, classification_report
 cnvrg_workdir = os.environ.get("CNVRG_WORKDIR", "/cnvrg")
 
+import tensorflow as tf
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+from tensorflow.keras.callbacks import *
+from tensorflow.keras.optimizers import *
 
-FILES = ['endpoint_test_data.csv', 'cnn_model_1.h5']
+from sklearn.preprocessing import StandardScaler
+import pathlib
+import requests
+import base64
+import json
+from io import BytesIO
 
-BASE_FOLDER_URL = "https://libhub-readme.s3.us-west-2.amazonaws.com/cnvrg/remaining_useful_life_data/"
+FILES = ['cnn_model.h5', 'shape_data.csv']
+
+BASE_FOLDER_URL = "https://libhub-readme.s3.us-west-2.amazonaws.com/remaining_useful_life_data/"
 
 
-def download_model_files():
+def download_model_files(FILES):
     """
     Downloads the model files if they are not already present or
     pulled as artifacts from a previous train task
@@ -41,44 +48,22 @@ def download_model_files():
                 fb.write(response.content)
 
 
-download_model_files()
+download_model_files(FILES)
 
-
-def data_preprocessing(x_test_data):
+def data_preprocessing(x_test_data, num_col, seq_len):
     '''
-
     Parameters
     ----------
     x_test_data : test data
+    num_col : numeric columns
 
-    Returns
-    -------
-    x_test_data : preprocessed data
-
+    Yields
+    ------
+    num_col : prerpocessed data
     '''
-
     test_df = x_test_data
-    print(test_df.shape)
     test_df.head(3)
-    print(test_df['s6'])
-
-    id_col = test_df.loc[0, 'id']
-    ### SCALE TEST DATA ###
-    numeric_features = []
-    for col in test_df.columns:
-        if col[0] == 's':
-            numeric_features.append(col)
-    scaler = StandardScaler()
-    test_df[numeric_features] = scaler.fit_transform(test_df[numeric_features])
-    #sensor_6 = test_df[['s6','s10']]
-    test_df = test_df[[i for i in test_df if len(set(test_df[i])) > 1]]
-    #test_df[['s6','s10']] = sensor_6
-#    test_df = test_df.loc[:,test_df.apply(pd.Series.nunique) != 1]
-    print(test_df.head())
-    print('===================================')
-    print(test_df.columns)
-    sequence_length = 50
-    test_df['id'] = id_col
+    sequence_length = seq_len
 
     def gen_sequence(id_df, seq_length, seq_cols):
 
@@ -96,81 +81,110 @@ def data_preprocessing(x_test_data):
                                      seq_length), range(seq_length, num_elements)):
             yield data_matrix[start:stop, :]
 
-    ### SEQUENCE COL: COLUMNS TO CONSIDER ###
-    sequence_cols = []
-    for col in test_df.columns:
-        if col[0] == 's':
-            sequence_cols.append(col)
-    print(sequence_cols)
-
     ### GENERATE X TRAIN TEST ###
-    test_data = []
+    x_test_data = []
     for engine_id in test_df['id'].unique():
         for sequence in gen_sequence(
-                test_df[test_df.id == engine_id], sequence_length, sequence_cols):
-            test_data.append(sequence)
+                test_df[test_df.id == engine_id], sequence_length, num_col):
+            x_test_data.append(sequence)
 
-    test_data = np.asarray(test_data)
+    x_test_data = np.asarray(x_test_data)
 
-    return test_data
-
-
-########## Path changing (whether the input comes from training or non-tra
-if os.path.exists("/input/cnn/cnn_model.h5"):
-    print('Running Training Inference')
-    cnn = "/input/cnn/cnn_model.h5"
-    shape_data = pd.read_csv("/input/data_preprocessing/shape_data.csv")
-
-else:
-    print('Running Stand Alone Endpoint')
-    script_dir = pathlib.Path(__file__).parent.resolve()
-    #x_test = pd.read_csv(os.path.join(script_dir,'raw_test_data.csv'))
-    cnn = os.path.join(script_dir, 'cnn_model.h5')
+    return x_test_data
 
 
-def predict(data):
+def rec_plot(s, eps=0.10, steps=10):
+    '''
+    generate reccurence plots
+    '''
+    d = pdist(s[:, None])
+    d = np.floor(d / eps)
+    d[d > steps] = steps
+    Z = squareform(d)
+    return Z
+
+
+def scaling_data(raw_test_data, num_feat, meta_cols):
     '''
     Parameters
     ----------
-    data : raw data from user
+    raw_test_data : raw_test_data
+    num_feat : numeric features
+    meta_cols : meta columns
 
     Returns
     -------
-    JSON file with predictions for remaining life of the machine
+    raw_test_data : scaled data
+    seq_cols : columns to be fed to the model
 
     '''
-    print(data)
-    x_test = data
-    #df = base64.b64decode(data)
-    #df = df.decode('utf-8').splitlines()
-    #df = json.loads(json.dumps(data))
-    #x_test = pd.read_csv(BytesIO(df))
-    x_test = data_preprocessing(x_test)
-    print(x_test.shape)
+    scaler = StandardScaler()
+    raw_test_data[num_feat] = scaler.fit_transform(
+        raw_test_data[num_feat])
+    raw_test_data = raw_test_data.loc[:,
+                                      raw_test_data.apply(pd.Series.nunique) != 1]
+    seq_cols = list(set(raw_test_data.columns.tolist()) - set(meta_cols))
 
-    def rec_plot(s, eps=0.10, steps=10):
-        d = pdist(s[:, None])
-        d = np.floor(d / eps)
-        d[d > steps] = steps
-        Z = squareform(d)
-        return Z
+    return raw_test_data, seq_cols
+########## Path changing (whether the input comes from training or non-training ##########
 
-    x_test_img = np.apply_along_axis(rec_plot, 1, x_test).astype('float16')
+def predict(data):
+### SCALE TEST DATA ###
+    print('Running Stand Alone Endpoint')
+    script_dir = pathlib.Path(__file__).parent.resolve()
+    test_file = list(str(data['file']))
+    download_model_files(test_file)
 
-    print(x_test_img.shape)
+    #x_test = pd.read_csv(os.path.join(script_dir,'endpoint_test_data.csv'))
+    x_test = pd.read_csv(os.path.join(script_dir,str(data['file'])))
+    cnn = os.path.join(script_dir,'cnn_model.h5')
+    cols = pd.read_csv(os.path.join(script_dir,'shape_data.csv'))
+    numeric_features = cols['cols'].tolist()
+    meta_columns = cols['meta_cols'].tolist()
+    seq_len = int(cols.loc[0,'sequence_length'])
+    upper_limit = int(cols.loc[0, 'cycles'])
+    lower_limit = int(cols.loc[1, 'cycles'])
+    id_col = x_test.loc[0,'id']
+    
+    #predict(x_test, numeric_features, meta_columns, seq_length)
+    test_data, sequence_cols = scaling_data(
+        x_test, numeric_features, meta_columns)
+    result_df = pd.DataFrame()
+    print(test_data.columns)
+    dic = {}
+    #for i in test_data.id.unique():
+        # for each id in test data
+    try:
+        #x_test = test_data[test_data['id'] == i]
+        x_test.reset_index(inplace=True)
 
-    from tensorflow.keras.models import load_model
-    # load model
-    model = load_model(cnn)
+        data = data_preprocessing(x_test, sequence_cols, seq_len)
+        print(data.shape)
+        x_test_img = np.apply_along_axis(
+            rec_plot, 1, data).astype('float16')
 
-    model.predict(x_test_img)
-    print(model.summary())
-    cnn_predicted = model.predict(x_test_img)
-    cnn_predicted_df = pd.DataFrame(cnn_predicted)
-    result = cnn_predicted_df.iloc[-1, :]
-    #response = cnn_predicted_df.to_json(orient='columns')
-    response = result.to_json(orient='columns')
-    print(response)
-    return response
-    # cnn_predicted_df.to_csv(cnvrg_workdir+"/cnn_predicted_data.csv",
-    # index=False) = "x_test"
+        # load model
+        model = load_model(cnn)
+
+        # predict
+        model.predict(x_test_img)
+        cnn_predicted = model.predict(x_test_img)
+        cnn_predicted_df = pd.DataFrame(cnn_predicted)            
+        cnn_predicted_df.rename(columns = {0:'>{} Cycles'.format(upper_limit), 1:'{}-{} Cycles'.format(lower_limit,upper_limit), 2:'<{} Cycles'.format(lower_limit)}, inplace = True)
+
+        cnn_predicted_df[cnn_predicted_df.columns] = round(cnn_predicted_df[cnn_predicted_df.columns]*100,2)
+        result = cnn_predicted_df.iloc[-1, :].idxmax()
+        if result == '>{} Cycles'.format(upper_limit):
+            j = 'More than {} cycles left'.format(upper_limit)
+        elif result == '{}-{} Cycles'.format(lower_limit,upper_limit):
+            j = '{} to {} cycles left'.format(lower_limit,upper_limit)
+        elif result == '<{} Cycles'.format(lower_limit):
+            j = 'Less than {} cycles left'.format(lower_limit)
+        dic = {'id': str(id_col) , 'prediction': j}
+        print(dic)
+        
+    except BaseException as e:
+        print(e)
+        print('machine has less rows than the defined sequence length')
+        dic = {'id': str(id_col) , 'prediction': 'machine has less rows than the defined sequence length', 'error': str(e)}
+    return dic
